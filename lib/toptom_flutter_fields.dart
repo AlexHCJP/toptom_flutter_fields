399c1a680eb48897dfc52a5e27f7b19d7c877686
AlexHCJP <alexganbert@gmail.com>
2023-10-02 12:19:33 +0600

    export

--- lib/toptom_flutter_fields.dart
+++ lib/toptom_flutter_fields.dart
@@ -5,4 +5,7 @@ export 'fields/number_field.dart';
 export 'fields/password_field.dart';
 export 'fields/phone_field.dart';
 export 'fields/pincode_field.dart';
+export 'fields/text_field.dart';
+export 'fields/email_field.dart';
+
 
