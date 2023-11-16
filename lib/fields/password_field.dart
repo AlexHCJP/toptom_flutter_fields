e3a639c7cd6b2af9543e30f26dffcfc3cc5c5d2b
licantropy <licantropy96@gmail.com>
2023-11-16 14:37:00 +0600

    feat: added obscure icon into password_field.dart

--- lib/fields/password_field.dart
+++ lib/fields/password_field.dart
@@ -1,37 +1,54 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import '../top_field.dart';
 
-class ToptomPasswordField extends StatelessWidget {
+class ToptomPasswordField extends StatefulWidget {
   final TextEditingController controller;
   final String? label;
   final String? hintText;
   final bool isRequired;
+  final String visibilityIcon;
+  final String visibilityOffIcon;
 
-  const ToptomPasswordField(
-      {super.key,
-      required this.controller,
-      this.label,
-      this.isRequired = false,
-      this.hintText});
+  const ToptomPasswordField({
+    super.key,
+    required this.controller,
+    this.label,
+    this.isRequired = false,
+    this.hintText,
+    required this.visibilityIcon,
+    required this.visibilityOffIcon,
+  });
+
+  @override
+  State<ToptomPasswordField> createState() => _ToptomPasswordFieldState();
+}
+
+class _ToptomPasswordFieldState extends State<ToptomPasswordField> {
+  bool obscureText = true;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        if (label != null) TopField(label: label!, isRequired: isRequired),
+        if (widget.label != null) TopField(label: widget.label!, isRequired: widget.isRequired),
         const SizedBox(height: 5),
         SizedBox(
           child: TextField(
-            controller: controller,
-            obscureText: true,
+            controller: widget.controller,
+            obscureText: obscureText,
             obscuringCharacter: '*',
             decoration: InputDecoration(
-              hintText: hintText,
+              hintText: widget.hintText,
               contentPadding: const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
               border: OutlineInputBorder(borderRadius: BorderRadius.circular(8)),
+              suffixIcon: GestureDetector(
+                onTap: () => setState(() => obscureText = !obscureText),
+                child: SvgPicture.asset(obscureText ? widget.visibilityIcon : widget.visibilityOffIcon),
+              ),
             ),
           ),
         ),
