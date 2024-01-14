46aa25478fa6d7d45faa76f81e569adc165bb383
AlexHCJP <alexganbert@gmail.com>
2024-01-14 17:55:33 +0600

    change export

--- lib/toptom_flutter_fields.dart
+++ lib/toptom_flutter_fields.dart
@@ -1,11 +1,5 @@
 library toptom_flutter_fields;
 
-export 'fields/description_field.dart';
-export 'fields/number_field.dart';
-export 'fields/password_field.dart';
-export 'fields/phone_field.dart';
-export 'fields/pincode_field.dart';
-export 'fields/text_field.dart';
-export 'fields/email_field.dart';
+export 'fields/fields.dart';
 
 
